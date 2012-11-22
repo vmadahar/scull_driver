6c2c78f88cfc677f6333087268204d301e8fa182
Varun Madahar <vmadahar@jetheaddev.com>
2012-11-21 17:57:45 -0800

    - Realling adding the scripts and changes to unregister.

--- scull.c
+++ scull.c
@@ -1,17 +1,41 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
 
+#define SCULL_DEVICE_NAME "scull_driver"
+
+typedef struct scull_s
+{
+	dev_t dev;
+	int major;
+	int minor;
+} scull_t;
+
+scull_t scull;
+
 static int __init scull_init(void)
 {
-	printk("Hello, world!\n");
+	int result = 0;	
+	result = alloc_chrdev_region(&scull.dev, 0, 1, SCULL_DEVICE_NAME);
+	if (result)
+	{
+		printk(KERN_ALERT "Error while registering the device\n");
+		return result;
+	}
+	scull.major = MAJOR(scull.dev);
+	scull.minor = MINOR(scull.dev);
+	printk("Hello, world!\n");	
+	printk("scull_driver major: %u minor: %u dev: %u\n", scull.major, scull.minor,scull.dev);
 	return 0;
 }
 
 static void __exit scull_exit(void)
 {
 	printk("Goodbye, cruel world!\n");
+	printk("Removing device: %u\n", scull.major);
+	unregister_chrdev_region(scull.dev,1);
 }
 
 module_init(scull_init);
--- scull_load.sh
+++ scull_load.sh
@@ -6,14 +6,14 @@ mode="664"
 
 /sbin/insmod ./$module.ko $* || exit 1
 
-rm -rf /dev/${device}0
+rm -rf /dev/${device}*
 
-major=$(awk "\\$2==\ "$module\" {print \\$1}" /proc/devices}
+#major=$(awk "\\$2==\"$module\" {print \\$1}" /proc/devices)
 
-mknod /dev/${device}0 c $major 0
+mknod /dev/${device} c 246 0
 
 group="staff"
 grep -q '^staff:' /etc/group || group="wheel"
 
-chgrp $group /dev/${device}0
-chmod $mode /dev/${device}0
+chgrp $group /dev/${device}
+chmod $mode /dev/${device}
